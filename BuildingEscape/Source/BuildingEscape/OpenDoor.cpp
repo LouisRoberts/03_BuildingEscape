9b2c7f0474a1d8fb54bfe98053d332f45dd855bc
Louis <louisjroberts@gmail.com>
2018-01-03 20:55:31 +0000

    Made trigger

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -22,7 +22,7 @@ void UOpenDoor::BeginPlay()
 	// ...
 	AActor* Owner = GetOwner();
 	// Create a rotator
-	FRotator NewRotation = FRotator(0.0f,-60.0f, 0.0f);
+	FRotator NewRotation = FRotator(0.0f,70.0f, 0.0f);
 
 	// Set the Door Rotation
 	Owner->SetActorRotation(NewRotation);
